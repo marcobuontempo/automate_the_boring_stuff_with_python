0c0dc5d54992d043fb0e60520209151d1b6a4a4d
marcobuontempo <marco.buon36@gmail.com>
2022-11-10 18:26:02 +1100

    Create 'form_filler.py' demo project

--- 18. Controlling the Keyboard and Mouse with GUI Automation/mouse_now.py
+++ 18. Controlling the Keyboard and Mouse with GUI Automation/mouse_now.py
@@ -10,6 +10,10 @@ try:
         # Get and print the mouse coordinates
         x, y = pyautogui.position()
         position_str = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+        pixel_colour = pyautogui.screenshot().getpixel((x,y))
+        position_str += " RGB: (" + str(pixel_colour[0]).rjust(3)
+        position_str += ", " + str(pixel_colour[1]).rjust(3)
+        position_str += ", " + str(pixel_colour[2]).rjust(3) + ")"
         print(position_str, end="")
         print("\b" * len(position_str), end="", flush=True)
 except KeyboardInterrupt:
